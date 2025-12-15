8b0a8a7aa291616adff804fd6afc7899391805ec
bala-amypo <bala@amypo.in>
2025-12-15 10:30:13 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,12 +1,19 @@
 package com.example.demo.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
 
-@service
+@Service
 public class UserServiceImpl implements UserService{
+    List<User> users = new ArrayList<>();
+    int idx = 1;
     User register(User user){
-
+        users.add(user);
     }
     User findByEmail(String email){
 
