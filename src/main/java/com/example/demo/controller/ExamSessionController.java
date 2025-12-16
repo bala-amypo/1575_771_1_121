17c10cf407ceb4f7bbf6af5fa37df9f4bce52c3f
bala-amypo <bala@amypo.in>
2025-12-16 14:19:45 +0000

    User commit

--- src/main/java/com/example/demo/controller/ExamSessionController.java
+++ src/main/java/com/example/demo/controller/ExamSessionController.java
@@ -1,18 +1,18 @@
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/sessions")
 public class ExamSessionController{
 
     @Autowired
     ExamSessionService examSessionService;
 
     @PostMapping("/")
-    public ResponseEntity<ExamSession> (@RequestBody ExamSession examSession){
-        return ResponseEntity.status(200).body(examSessionService.admSession(examSession));
+    public ResponseEntity<ExamSession> createSession(@RequestBody ExamSession examSession){
+        return ResponseEntity.status(200).body(examSessionService.createSession(examSession));
     }
 
-    @GetMapping("/")
-    public ResponseEntity<ExamSession> lismSessions(@RequestBody ExamSession examSession){
-        return ResponseEntity.status(200).body(examSessionService.getAllStudents());
+    @GetMapping("/{sessionId}")
+    public ResponseEntity<ExamSession> getSession(@PathVariable sessionId){
+        return ResponseEntity.status(200).body(examSessionService.getSession(sessionId));
     }
 }
