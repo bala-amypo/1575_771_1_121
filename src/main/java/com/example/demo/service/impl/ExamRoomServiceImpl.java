dd60d4635e3677e45495347014c2406969f3a957
bala-amypo <bala@amypo.in>
2025-12-16 12:42:52 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ExamRoomServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ExamRoomServiceImpl.java
@@ -4,7 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo1.model.ExamRoom;
-@Repository
-public interface ExamRoomRepository extends JpaRepository<ExamRoom, Long>{
-
+@Service
+public interface ExamRoomServiceImpl implements ExamRoomService{
+    @Autowiring
+    res
 }
