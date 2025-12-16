565bc70b7c23f45954ae679d16e862ea9ccdfaf5
bala-amypo <bala@amypo.in>
2025-12-16 13:36:37 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -6,12 +6,12 @@ public class AuthController{
     StudentService studentService;
 
     @PostMapping("/addstudent")
-    public ResponseEntity<User>addStudent(@Valid @RequestBody Student student){
-        return ResponseEntity.status(201).body(userService.addStudent(user));
+    public ResponseEntity<Student>addStudent(@Valid @RequestBody Student student){
+        return ResponseEntity.status(201).body(StudentService.addStudent(student));
     }
 
     @PostMapping("/liststudents")
-    public ResponseEntity<User>login(@RequestBody String email){
-        return ResponseEntity.status(200).body(userService.findByEmail(email));
+    public ResponseEntity<Student> getAllStudents(){
+        return ResponseEntity.status(200).body(StudentService.getAllStudents());
     }
 }
