7b8233a4dc7f82a1b2a8aab63f6b86240178d4d7
bala-amypo <bala@amypo.in>
2025-12-17 04:27:43 +0000

    User commit

--- src/main/java/com/example/demo/repository/SeatingPlanRepository.java
+++ src/main/java/com/example/demo/repository/SeatingPlanRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.example.demo.model.SeatingPlan;
 
 @Repository
 public interface SeatingPlanRepository extends JpaRepository<SeatingPlan,Long>{
+
     List<SeatingPlan> findByExamSessionId(long sessionId);
+
 }
