d03c2261fd9bdc19e8bce6a36d8bc0a576173978
bala-amypo <bala@amypo.in>
2025-12-17 16:56:17 +0000

    User commit

--- src/main/java/com/example/demo/service/ExamSessionService.java
+++ src/main/java/com/example/demo/service/ExamSessionService.java
@@ -1,12 +1,9 @@
 package com.example.demo.service;
 
-
-import java.util.Optional;
-
 import com.example.demo.model.ExamSession;
 
 
 public interface ExamSessionService{
     ExamSession createSession(ExamSession session);
-    Optional<ExamSession> getSession(long sessionId);
+    ExamSession getSession(long sessionId);
 }
