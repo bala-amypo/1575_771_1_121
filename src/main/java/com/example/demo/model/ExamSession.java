777210e45913cc336175cf8caba12648c699abf5
bala-amypo <bala@amypo.in>
2025-12-18 08:55:08 +0000

    User commit

--- src/main/java/com/example/demo/model/ExamSession.java
+++ src/main/java/com/example/demo/model/ExamSession.java
@@ -1,8 +1,8 @@
 package com.example.demo.model;
-jakarta.persistence.CascadeType
+import jakarta.persistence.CascadeType;
 import java.time.LocalDate;
 import java.util.Set;
-jakarta.persistence.Column
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.persistence.GeneratedValue;
