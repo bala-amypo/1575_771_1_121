998930d64cc69098c0005fe93a20ba02e41cc786
bala-amypo <bala@amypo.in>
2025-12-19 06:15:15 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtTokenProvider.java
+++ src/main/java/com/example/demo/security/JwtTokenProvider.java
@@ -1,1 +1,11 @@
 package com.example.demo.security;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtTokenProvider {
+    // This can be empty for now just to fix the "cannot find symbol" error
+    public String generateToken(String username) {
+        return "dummy-token";
+    }
+}
