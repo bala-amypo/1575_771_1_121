16964014ab5b0837ff99b063538e9b20f364a7e3
bala-amypo <bala@amypo.in>
2025-12-26 09:05:29 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -33,6 +33,6 @@ public class OpenApiConfig {
                                                 .scheme("bearer")
                                                 .bearerFormat("JWT")
                                 )
-                )
+                );
     }
 }
